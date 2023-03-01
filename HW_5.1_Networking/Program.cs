fb1e0c9fe056ea30e74b391fa1ed3e92db2a6d34
Ihor Arkhypenko <wasbornindecember@gmail.com>
2023-03-01 18:36:36 +0200

    New project for developing created, .gitignore file added

---
+++ HW_5.1_Networking/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HW_5._1_Networking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
